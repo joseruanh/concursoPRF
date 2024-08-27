63bcf356a75031d25aefe4d7bf5648b31e540222
Ruanh <79330021+joseruanh@users.noreply.github.com>
2024-08-27 09:59:19 -0300

    att

--- js/script.js
+++ js/script.js
@@ -1,5 +1,60 @@
+function topo()
+{
+    window.scrollTo(0,0);
+}
+
+function conteudos()
+{
+    window.scrollTo(0,0);
+    let a = window.document.getElementById("conteudo")
+    a.innerHTML = `
+    
+
+    <div class="materias">
+
+        <div class="disciplina" id="port" onclick="portugues()">
+            <p> Português </p>
+        </div>
+
+        <div class="disciplina" id="dadm" onclick="dadm()">
+            <p> Direito Administrativo </p>
+        </div>
+
+        <div class="disciplina" id="dcons">
+            <p> Direito Constitucional </p>
+        </div>
+
+        <div class="disciplina" id="info">
+            <p> Informática </p>
+        </div>
+
+        <div class="disciplina" id="raclog">
+            <p> Raciocínio Lógico </p>
+        </div>
+
+        <div class="disciplina" id="dpen">
+            <p> Direito Penal </p>
+        </div>
+
+        <div class="disciplina" id="ctb">
+            <p> Códito de Trânsito Brasileiro (CTB) </p>
+        </div>
+
+        <div class="disciplina" id="dirpp">
+            <p> Direito Processual Penal </p>
+        </div>
+
+        
+    
+    </div>
+    
+    
+    `
+}
+
 function portugues()
 {
+    window.scrollTo(0,0);
     let a = window.document.getElementById("conteudo")
     a.innerHTML = `
 
@@ -11,10 +66,10 @@ Vamos abordar o conteúdo de Gramática para o concurso da PRF com foco em Class
 
 <br><br>
 
-1. Classes de Palavras <br>
+<h4>1. Classes de Palavras</h4>
 As classes de palavras são categorias gramaticais que agrupam palavras com características semelhantes. No português, temos 10 classes gramaticais: substantivo, adjetivo, artigo, numeral, pronome, verbo, advérbio, preposição, conjunção e interjeição.<br> 
 <br>
-1.1. Substantivo<br>
+<h4>1.1. Substantivo</h4>
 Definição: Palavra que dá nome a seres, objetos, lugares, sentimentos, etc.<br>
 Tipos:<br>
 Comum: designa seres da mesma espécie. Ex.: carro, cidade.<br>
@@ -22,23 +77,23 @@ Próprio: designa seres específicos. Ex.: Brasil, Maria.<br>
 Concreto: existe por si só. Ex.: pedra, água. <br>
 Abstrato: depende de outros seres para existir, como sentimentos e ações. Ex.: amor, corrida. <br>
 Coletivo: designa um grupo de seres da mesma espécie. Ex.: alcateia (grupo de lobos). <br> <br>
-1.2. Adjetivo <br>
+<h4>1.2. Adjetivo</h4>
 Definição: Palavra que qualifica ou caracteriza um substantivo. <br>
 Exemplo: carro rápido, pessoa inteligente. <br>
 Flexão: gênero (masculino/feminino), número (singular/plural) e grau (comparativo/superlativo). <br><br>
-1.3. Artigo <br>
+<h4>1.3. Artigo </h4>
 Definição: Palavra que antecede o substantivo para determiná-lo ou indeterminá-lo. <br>
 Tipos: <br>
 Definido: o, a, os, as. Ex.: A casa. <br>
 Indefinido: um, uma, uns, umas. Ex.: Uma casa. <br><br>
-1.4. Numeral <br>
+<h4>1.4. Numeral </h4>
 Definição: Palavra que indica quantidade, ordem, fração ou multiplicação. <br>
 Tipos: <br>
 Cardinal: um, dois, três. <br>
 Ordinal: primeiro, segundo, terceiro. <br>
 Multiplicativo: dobro, triplo. <br>
 Fracionário: meio, terço. <br><br>
-1.5. Pronome <br>
+<h4>1.5. Pronome </h4>
 Definição: Palavra que substitui ou acompanha o substantivo. <br>
 Tipos: <br>
 Pessoais: eu, tu, ele, nós, vós, eles. <br>
@@ -47,29 +102,29 @@ Possessivos: meu, teu, seu, nosso, vosso. <br>
 Indefinidos: algum, nenhum, todo. <br>
 Relativos: que, quem, cujo. <br>
 Interrogativos: qual, quem, quantos. <br><br>
-1.6. Verbo <br>
+<h4>1.6. Verbo </h4>
 Definição: Palavra que indica ação, estado ou fenômeno. <br>
 Conjugação: presente, passado, futuro, modos (indicativo, subjuntivo, imperativo). <br>
 Exemplo: Eu estudo, tu corres. <br><br>
-1.7. Advérbio <br>
+<h4>1.7. Advérbio </h4>
 Definição: Palavra que modifica o verbo, o adjetivo ou outro advérbio. <br>
 Tipos: modo, tempo, lugar, negação, afirmação, dúvida. <br>
 Exemplo: rapidamente, ontem, aqui, não, sim, talvez. <br><br>
-1.8. Preposição <br>
+1.8. Preposição </h4>
 Definição: Palavra que liga dois termos, estabelecendo uma relação entre eles. <br>
 Exemplo: de, em, por, para, com. <br>
 Uso: livro de João, vou para casa. <br><br>
-1.9. Conjunção <br>
+<h4>1.9. Conjunção </h4>
 Definição: Palavra que liga orações ou termos semelhantes em uma oração. <br>
 Tipos: coordenativas (e, mas, ou) e subordinativas (quando, se, porque). <br>
 Exemplo: Eu estudo e trabalho. <br><br>
-1.10. Interjeição <br>
+1.10. Interjeição </h4>
 Definição: Palavra que expressa emoção, estado de espírito ou que tenta reproduzir sons. <br>
 Exemplo: Oh!, Ah!, Socorro!, Ufa! <br><br>
-2. Concordância Verbal <br>
+<h4>2. Concordância Verbal </h6>
 A concordância verbal ocorre quando o verbo se ajusta ao sujeito da oração em número e pessoa. <br>
  <br><br>
-2.1. Regras Gerais <br>
+<h4>2.1. Regras Gerais </h4>
 Sujeito Simples: O verbo concorda com o núcleo do sujeito em número e pessoa. <br>
  <br>
 Exemplo: O aluno estuda. (sujeito singular) / Os alunos estudam. (sujeito plural). <br>
@@ -79,17 +134,17 @@ Exemplo: Maria e João chegaram cedo. <br>
 Sujeito Composto Posposto ao Verbo: O verbo pode concordar com o núcleo mais próximo ou ir para o plural. <br>
  <br>
 Exemplo: Chegou Maria e João. ou Chegaram Maria e João. <br><br>
-2.2. Casos Especiais <br>
+<h4>2.2. Casos Especiais </h4>
 Coletivo: Verbo no singular. <br>
 Exemplo: A multidão gritou. <br>
 Sujeito Inexistente (verbo impessoal): Verbo sempre na terceira pessoa do singular. <br>
 Exemplo: Choveu muito ontem. <br>
 Pronome de Tratamento: O verbo concorda com a terceira pessoa do singular ou plural. <br>
 Exemplo: Vossa Excelência está certa. / Vossas Excelências estão certos. <br><br>
-3. Concordância Nominal <br>
+<h4>3. Concordância Nominal </h4>
 A concordância nominal ocorre quando adjetivos, artigos, numerais e pronomes se ajustam ao substantivo em gênero e número. <br>
  <br><br>
-3.1. Regras Gerais <br>
+<h4>3.1. Regras Gerais </h4>
 Adjetivo Concorda com o Substantivo: Em gênero e número. <br>
  <br>
 Exemplo: A casa bonita. / Os carros bonitos. <br>
@@ -100,7 +155,7 @@ Se estiver no plural, concorda com todos os substantivos. Exemplo: Carro e moto 
 Pronomes: Concordam com o substantivo que acompanham. <br>
  <br>
 Exemplo: Meu livro, minhas canetas. <br><br>
-3.2. Casos Especiais <br>
+<h4>3.2. Casos Especiais </h4>
 Adjetivo Anteposto a Substantivos de Gêneros Diferentes: Concorda com o mais próximo. <br>
  <br>
 Exemplo: Bonito carro e moto. <br>
@@ -111,7 +166,7 @@ Expressões “É bom”, “É necessário”, “É proibido”: <br>
  <br>
 Sem artigo: invariável. Exemplo: É proibido entrada. <br>
 Com artigo: concorda. Exemplo: É proibida a entrada. <br><br>
-Dicas de Estudo <br>
+<h4>Dicas de Estudo </h4>
 Resumos e Mapas Mentais: Faça resumos dos tópicos e crie mapas mentais para visualizar melhor as conexões entre os conceitos. <br>
 Prática: Resolva exercícios de provas anteriores e simulados focados na PRF. <br>
 Leitura: Leia textos variados e identifique as regras gramaticais aplicadas, especialmente concordância. <br>
@@ -122,8 +177,9 @@ Revisão: Revise constantemente, intercalando os tópicos para garantir uma melh
 
 }
 
-function direitoadm()
+function dadm()
 {
+    window.scrollTo(0,0);
     let a = window.document.getElementById("conteudo")
     a.innerHTML = `
 
@@ -131,13 +187,13 @@ function direitoadm()
     
     Vamos lá! Vou fornecer um conteúdo detalhado para você estudar os temas de "Organização da Administração Pública", "Poderes Administrativos" e "Licitações" em Direito Administrativo, focado para o concurso da PRF. Vou dividir em tópicos para facilitar o estudo. <br>
 <br>
-1. Organização da Administração Pública<br>
-1.1. Conceito e Estrutura da Administração Pública<br>
+<h4>1. Organização da Administração Pública</h4>
+<h4>1.1. Conceito e Estrutura da Administração Pública</h4>
 Administração Pública: Conjunto de órgãos, agentes e atividades estatais, destinadas a assegurar os interesses públicos.<br>
 Estrutura:<br>
 Administração Direta: Constituída pelos órgãos que integram as pessoas políticas (União, Estados, Distrito Federal e Municípios). Ex.: Ministérios, Secretarias.<br>
 Administração Indireta: Formada por pessoas jurídicas que desempenham atividades descentralizadas. Ex.: Autarquias, Fundações, Empresas Públicas, Sociedades de Economia Mista.<br><br>
-1.2. Descentralização e Concentração<br>
+<h4>1.2. Descentralização e Concentração</h4>
 Descentralização: Transferência de atividades de um ente para outro, mantendo o primeiro o controle e fiscalização.<br><br>
 <br>
 Política: Criação de entidades federativas autônomas.<br>
@@ -146,31 +202,31 @@ Territorial: Criação de territórios ou entidades que gerem determinados locai
 Por Serviço: Criação de uma entidade para executar determinada função (ex.: INSS).<br>
 Concentração: União de competências em uma única entidade ou órgão, sem divisão de funções.<br><br>
 <br>
-1.3. Desconcentração<br>
+<h4>1.3. Desconcentração</h4>
 Desconcentração: Distribuição interna de competências dentro de uma mesma entidade. Ex.: Departamentos, Divisões dentro de um Ministério.<br><br>
-1.4. Entidades da Administração Indireta<br>
+<h4>1.4. Entidades da Administração Indireta</h4>
 Autarquias: Entidades autônomas criadas por lei para desempenhar funções típicas do Estado. Ex.: INSS, ANVISA.<br>
 Fundações Públicas: Entidades criadas para fins específicos de interesse público, podendo ser públicas ou privadas. Ex.: FUNAI.<br>
 Empresas Públicas e Sociedades de Economia Mista: Atuam no mercado, mas sob controle estatal. Ex.: Correios (Empresa Pública), Petrobras (Sociedade de Economia Mista).<br><br>
-2. Poderes Administrativos<br>
-2.1. Conceito<br>
+<h4>2. Poderes Administrativos</h4>
+<h4>2.1. Conceito</h4>
 Poderes Administrativos: Instrumentos que a Administração Pública possui para a realização de suas funções. São poderes-deveres, ou seja, devem ser exercidos em prol do interesse público.<br><br>
-2.2. Poder Hierárquico<br>
+<h4>2.2. Poder Hierárquico</h4>
 Poder Hierárquico: Relação de subordinação entre os diferentes órgãos e agentes da Administração. Permite que superiores possam dar ordens, fiscalizar, rever atos e aplicar sanções aos subordinados.<br>
 Delegação e Avocação: Delegar é transferir competência, enquanto avocar é chamar para si a competência de um subordinado.<br><br>
-2.3. Poder Disciplinar<br>
+<h4>2.3. Poder Disciplinar</h4>
 Poder Disciplinar: Poder da Administração de aplicar sanções a servidores públicos e particulares com vínculo contratual com o Estado, quando infringem normas legais ou regulamentares.<br>
 Diferença com o Poder Punitivo Geral: Este último aplica-se a toda a sociedade (ex.: Direito Penal), enquanto o Poder Disciplinar aplica-se somente àqueles em relação de sujeição especial com o Estado.<br><br>
-2.4. Poder Regulamentar<br>
+<h4>2.4. Poder Regulamentar</h4>
 Poder Regulamentar: Poder que a Administração tem de editar atos normativos para complementar ou detalhar as leis, sem alterá-las.<br>
 Decretos Regulamentares: Editados pelo chefe do Executivo para garantir a fiel execução da lei.<br>
 Instruções Normativas e Portarias: Atos normativos expedidos por autoridades administrativas.<br><br>
-2.5. Poder de Polícia<br>
+<h4>2.5. Poder de Polícia</h4>
 Poder de Polícia: Poder da Administração de condicionar e restringir o exercício de direitos individuais em favor do interesse público.<br>
 Características: Autoexecutoriedade (capacidade de executar sem intervenção do Judiciário), Discricionariedade, Coercibilidade.<br>
 Limites: Deve observar a legalidade, proporcionalidade e o devido processo legal.<br><br>
-3. Licitações<br>
-3.1. Conceito e Princípios<br>
+<h4>3. Licitações</h4>
+<h4>3.1. Conceito e Princípios</h4>
 Licitação: Procedimento administrativo formal para selecionar a proposta mais vantajosa para a Administração, visando a contratação de obras, serviços, compras e alienações.<br>
 Princípios:<br>
 Legalidade: A licitação deve obedecer à lei.<br>
@@ -181,20 +237,20 @@ Publicidade: Os atos devem ser públicos.<br>
 Vinculação ao Instrumento Convocatório: A Administração deve seguir o que foi estabelecido no edital.<br>
 Julgamento Objetivo: As propostas devem ser julgadas de forma objetiva.<br>
 Competitividade: A licitação deve estimular a competição.<br><br>
-3.2. Modalidades de Licitação<br>
+<h4>3.2. Modalidades de Licitação</h4>
 Concorrência: Modalidade mais abrangente, para contratos de maior valor.<br>
 Tomada de Preços: Para contratos de médio valor, onde os licitantes devem estar previamente cadastrados.<br>
 Convite: Para contratos de menor valor, com convite direto a, pelo menos, três interessados.<br>
 Concurso: Para escolha de trabalho técnico, científico ou artístico.<br>
 Leilão: Para venda de bens móveis e imóveis inservíveis.<br>
 Pregão: Modalidade para aquisição de bens e serviços comuns.<br><br>
-3.3. Dispensa e Inexigibilidade de Licitação<br>
+<h4>3.3. Dispensa e Inexigibilidade de Licitação</h4>
 Dispensa: Situações em que a licitação é dispensada por lei (ex.: emergência, pequeno valor).<br>
 Inexigibilidade: Situações em que a licitação é inviável, como em casos de exclusividade de fornecedor.<br><br>
-3.4. Fases da Licitação<br>
+<h4>3.4. Fases da Licitação</h4>
 Fase Interna: Preparação do edital, definição do objeto, estudo de mercado.<br>
 Fase Externa: Publicação do edital, apresentação de propostas, julgamento, homologação e adjudicação.<br><br>
-3.5. Contratos Administrativos<br>
+<h4>3.5. Contratos Administrativos</h4>
 Características: São contratos regidos por normas de direito público, com cláusulas exorbitantes.<br>
 Exemplos de Cláusulas Exorbitantes: Prerrogativas da Administração de modificar unilateralmente o contrato, rescindir o contrato por interesse público, aplicar sanções.<br><br>
 Esse conteúdo abrange os principais pontos dos temas solicitados. Recomendo que você estude cada um desses tópicos com profundidade, utilizando materiais complementares como livros e legislações atualizadas, além de realizar exercícios para fixar o conteúdo.
@@ -204,5 +260,6 @@ Esse conteúdo abrange os principais pontos dos temas solicitados. Recomendo que
 
 function recarregar()
 {
+    window.scrollTo(0,0);
     window.location.reload();
 }
